96d5cec030d2e2ded32da70df6189e007b898d4b
jason wambua <jasonwamua31@gmail.com>
2025-10-05 17:25:25 +0300

    my first commit

---
+++ main.py
@@ -0,0 +1,48 @@
+print("hello world")
+course = 'jason is a programmer'
+print(course.upper())
+print(course.find('n'))
+print(course.replace('programmer', 'hacker'))
+print(3 ** 3)
+x = 5
+x = x + 10
+print(x)
+x = 5
+x += 10
+print(x)
+x = 5 <= 6
+print(x)
+age = 12
+if age >= 12:
+    print("not an adult")
+username = input("enter username")#username: jay_b
+password = input("enter passward")#password: jasonjoan
+if username == "jay_b" and password == "jasonjoan":
+    print("correct password")
+else : print("incorect")
+course = "today is tuseday"
+print(course.replace('tuesday' , 'friday'))
+age = 18
+if age >= 18:
+    print("adult")
+else : print("not adult")
+password = input("enter password :")# password 1234
+if password == "1234":
+    print("access granted")
+else : print("access denied") 
+exam_score = int(input("enter exam score :"))
+if exam_score >= 80:
+    print("Grade A")
+elif exam_score  >= 60:
+    print("Grade B")
+elif exam_score >= 40:
+    print("Grade C")
+else :
+    print("Fail")
+day = input("days of the week :")
+if day == ("monday" and "friday"):
+    print("weekday")
+elif day == "saturday" or day == "sunday" :
+    print("weeknd")
+else :
+    print("invald day")
